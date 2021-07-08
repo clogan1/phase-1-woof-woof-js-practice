4e8e4329111af90785e52f57fd265aafc005d208
Claire <clairelogan16@gmail.com>
2021-07-07 21:26:01 -0700

    added notes

--- src/index.js
+++ src/index.js
@@ -4,42 +4,45 @@
 
 //global variables
 
-let allDogs;
-let goodDogs;
+let allDogs; //so we can store an array of ALL dogs
+let goodDogs; //so we can store an array of only GOOD dogs
 
 //initial render
 function initialRender(){
-    fetchDogBarData()
+    fetchDogBarData() //adds dog names to the top bar
 
     //#good-dog-filter event listener 
     document.querySelector('#good-dog-filter').addEventListener('click', goodDogToggle)
 }
 
-initialRender()
+initialRender() //calls initial render
 
+
+//Fetches 
 
 //Fetch dogs for dog bar
 function fetchDogBarData(){
     fetch('http://localhost:3000/pups')
     .then(res => res.json())
-    .then(json => json.forEach(renderDogBar))
+    .then(json => json.forEach(renderDogBar)) //for each dog in the JSON, execute the renderDogBar function
 }
 
+//special fetch when GOOD dog filter is ON
 function fetchGoodDogBarData(){
     fetch('http://localhost:3000/pups')
     .then(res => res.json())
     .then(json => {
-        allDogs = json;
-        goodDogs = allDogs.filter((dog) => dog.isGoodDog === true)
-        goodDogs.forEach(renderDogBar)
+        allDogs = json;  //array of all dog 
+        goodDogs = allDogs.filter((dog) => dog.isGoodDog === true) //filtering all dogs and returning a new array with dogs that have "isGoodDog" : true in data
+        goodDogs.forEach(renderDogBar) //for each dog in this new good dog array, execute the renderDogBar function to update our dog bar with only good dogs
     })
 }
 
-//fetch dog page data
+//Fetch dog page data
 function fetchDogPageData(id)  {
-    fetch(`http://localhost:3000/pups/${id}`)
+    fetch(`http://localhost:3000/pups/${id}`) //will only pull that specific dog's data, based on id
     .then(res => res.json())
-    .then(json => renderDogPage(json))
+    .then(json => renderDogPage(json)) //execute the renderDogPage function passing the dog's specific dataset as the argument
 }
 
 
@@ -48,8 +51,7 @@ function fetchDogPageData(id)  {
 function renderDogBar(data){
     //create span elements for each dog
     let span = document.createElement('span')
-    let id = data.id
-    //console.log(id)
+    //console.log(data.id)
     span.className = "dogSpan"
     //span.id = data.id
     span.textContent = data.name
@@ -58,15 +60,14 @@ function renderDogBar(data){
     document.querySelector('#dog-bar').append(span)
 
     //add event listener to each Span
-    //callback function  ==> function renderDogPage()
     span.addEventListener('click', () => {
-        fetchDogPageData(data.id)
+        fetchDogPageData(data.id) //calls the fetchDogPageData function, passing the dog's specific ID (data.id) as the argument
     })
 
 }
 
 function renderDogPage(data){
-    //clear whatever content is in the #dog-info
+    //clear whatever content is in the #dog-info div
     document.querySelector('#dog-info').innerHTML = ''
 
     //create elements
@@ -85,7 +86,6 @@ function renderDogPage(data){
     document.querySelector('#dog-info').append(img, h2, btn)
 
     //add event listener to good / bad dog button
-    //callback function --> function buttonToggle()
 
     btn.addEventListener('click', () => {
         //1. changes text of button from good to bad
@@ -93,10 +93,7 @@ function renderDogPage(data){
             btn.textContent = "Bad Dog"
         } else {btn.textContent = "Good Dog"}
     //2. updates database with new status ("isGoodDog": true or false)
-       // data.id
-
-    
-
+       // Skip
     })
 }
 
@@ -104,8 +101,10 @@ function renderDogPage(data){
 //Event handlers
 
 function goodDogToggle(){
+    //declare variables for elements
     let toggle = document.querySelector('#good-dog-filter')
     let dogBar = document.querySelector('#dog-bar')
+
     //if textContent = "Filter good dogs: OFF" then update to "Filter good dogs: ON"
     //clear render dog bar
     //update with only the good dogs 
@@ -113,6 +112,7 @@ function goodDogToggle(){
         toggle.textContent = "Filter good dogs: ON";
         dogBar.innerHTML = ''
         fetchGoodDogBarData();
+        
     //else update textContent to "Filter good dogs: OFF"
     //clear render dog bar
     //update with only the bad dogs 
